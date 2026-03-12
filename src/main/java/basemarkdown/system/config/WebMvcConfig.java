5fbdb922e89080826a372adc4291f590f2699860
한돈희 <hdh2680@gmail.com>
2026-03-12 18:05:28 +0900

    login

--- src/main/java/basemarkdown/system/config/WebMvcConfig.java
+++ src/main/java/basemarkdown/system/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package basemarkdown.system.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -9,9 +8,4 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("html/main");
-    }
 }
